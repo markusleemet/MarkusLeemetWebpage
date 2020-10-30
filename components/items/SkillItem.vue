47022e51861d5fa10523b0da9e1069a170c5c194
Markus Leemet <markusleemet@gmail.com>
2020-10-30 11:17:24 +0200

    Create skillItem component

---
+++ components/items/SkillItem.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .skill-item-component.custom-item
+    b-img(src="~/assets/images/hexagon.png")
+    .skill-name-and-description
+      h4.custom-item-name {{ skillItem.name }}
+      img.skill-item-logo(:src="skillItem.icon")
+</template>
+
+<script lang="js">
+export default {
+  name: 'SkillItem',
+  props: {
+    skillItem: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.skill-item-component {
+  position: relative;
+  max-width: 200px;
+  margin: 0 !important;
+  img{
+    width: 100%;
+  }
+  .skill-name-and-description{
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  .skill-item-logo{
+    max-width: 25%;
+  }
+}
+</style>
--- components/sections/Skills.vue
+++ components/sections/Skills.vue
@@ -5,13 +5,62 @@
         h2 {{ $t('resume.sections.skills.title') }}
         b-img.section-image(src="~/assets/images/skills.png")
         ResumeMenu(selectedOption="Oskused")
-      b-col(cols="12" sm="6").information-container
-        p
+      b-col(cols="12" sm="4" offset-sm="1").information-container
+        h2.mb-5.text-center Kasutatud tehnoloogiad:
+        b-row
+          b-col.d-flex.flex-row.justify-content-center.skill-item-container(cols="12" xl="4" v-for="(skillItem, index) in skillItems" v-bind:class="{'offset-xl-2': skillItem.offset}")
+            SkillItem(:skillItem="skillItem")
+        h2.mb-5.text-center Keeled, mida oskan:
+        b-row.justify-content-center
+          b-col.d-flex.flex-row.justify-content-center.skill-item-container(cols="12" xl="4" v-for="(languageItem, index) in languageItems")
+            SkillItem(:skillItem="languageItem")
 </template>
 
-<script lang="ts">
+<script lang="js">
+import html from '~/assets/logos/html.png'
+import css from '~/assets/logos/css.png'
+import bootstrap from '~/assets/logos/bootstrap.png'
+import javascript from '~/assets/logos/javascript.png'
+import nuxt from '~/assets/logos/nuxt.png'
+import vue from '~/assets/logos/vue.png'
+import react from '~/assets/logos/react.png'
+import android from '~/assets/logos/android.png'
+import java from '~/assets/logos/java.png'
+import python from '~/assets/logos/python.png'
+import sql from '~/assets/logos/sql.png'
+import pug from '~/assets/logos/pug.png'
+import sass from '~/assets/logos/sass.png'
+import wordpress from '~/assets/logos/wordpress.png'
+import sendgrid from '~/assets/logos/sendgrid.png'
+import eesti from '~/assets/logos/eesti.png'
+import usFlag from '~/assets/logos/us_flag.png'
 export default {
-  name: 'Skills'
+  name: 'Skills',
+  data () {
+    return {
+      skillItems: [
+        { name: 'HTML', icon: html, offset: false },
+        { name: 'CSS', icon: css, offset: false },
+        { name: 'Bootstrap', icon: bootstrap, offset: false },
+        { name: 'JavaScript', icon: javascript, offset: true },
+        { name: 'Nuxt', icon: nuxt, offset: false },
+        { name: 'Vue', icon: vue, offset: false },
+        { name: 'React', icon: react, offset: false },
+        { name: 'Android', icon: android, offset: false },
+        { name: 'Java', icon: java, offset: true },
+        { name: 'Python', icon: python, offset: false },
+        { name: 'Sql', icon: sql, offset: false },
+        { name: 'Pug', icon: pug, offset: false },
+        { name: 'Sass', icon: sass, offset: false },
+        { name: 'Wordpress', icon: wordpress, offset: true },
+        { name: 'Sendgrid', icon: sendgrid, offset: false }
+      ],
+      languageItems: [
+        { name: 'Eesti', icon: eesti },
+        { name: 'English', icon: usFlag }
+      ]
+    }
+  }
 }
 </script>
 
@@ -19,6 +68,12 @@ export default {
 .skill-component{
   .menu-item{
     background-color: #B8F3FF;
+  }
+  .information-container{
+    .skill-item-container{
+      padding-right: 0.6rem;
+      padding-left: 0.6rem;
+    }
   }
 }
 </style>
